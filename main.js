9600d460dd8d2f4a71d09ac81afd46ab595e91a7
James Halliday <mail@substack.net>
2015-10-01 20:30:57 -0700

    sweet tabs

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
-var main = require('main-loop')
+var xtend = require('xtend')
 var render = require('./lib/render.js')
+
+var main = require('main-loop')
 var loop = main(
   { url: location.pathname },
   render, require('virtual-dom')
 )
 var root = document.querySelector('#content')
 root.replaceChild(loop.target, root.childNodes[0])
+
+var singlePage = require('single-page')
+var show = singlePage(function (href) {
+  loop.update(xtend({ url: href }))
+})
+require('catch-links')(window, show)
