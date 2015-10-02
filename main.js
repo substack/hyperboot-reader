ace78e943075be9379304a0d0af38fd675eaaca2
James Halliday <mail@substack.net>
2015-10-01 21:53:59 -0700

    svg icon stub

--- main.js
+++ main.js
@@ -1,16 +1,23 @@
 var xtend = require('xtend')
+var fs = require('fs')
 var render = require('./lib/render.js')
 
+var icons = {
+  hyperpaint: fs.readFileSync('public/hyperpaint.svg', 'base64')
+}
+
 var main = require('main-loop')
-var loop = main(
-  { url: location.pathname },
-  render, require('virtual-dom')
-)
+var loop = main({
+  url: location.pathname,
+  installed: [
+    { title: 'hyperpaint', icon: icons.hyperpaint }
+  ]
+}, render, require('virtual-dom'))
 var root = document.querySelector('#content')
 root.replaceChild(loop.target, root.childNodes[0])
 
 var singlePage = require('single-page')
 var show = singlePage(function (href) {
-  loop.update(xtend({ url: href }))
+  loop.update(xtend(loop.state, { url: href }))
 })
 require('catch-links')(window, show)
